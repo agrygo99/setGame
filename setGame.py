2cce6c3fcb710a98dd73cff17d06aa7dc919ac8b
agrygo99 <agrygo@uiowa.edu>
2020-09-08 08:54:30 -0500

    fix issues within finding set

--- setGame.py
+++ setGame.py
@@ -4,51 +4,133 @@ Created on Mon Aug 31 18:20:37 2020
 
 @author: ashle
 """
-#import tkinter
-#from tkinter import messagebox
-#import Card
+import tkinter
+from tkinter import messagebox
 
-#root= tkinter.Tk()
-#root.withdraw()
+root= tkinter.Tk()
+root.withdraw()
 
-#takes an input of 3 cards and compares all of their qualities to determine if they form a set or not
-#i added pop up windows to say if it is a good set, or an error for nonvalid set
-def isSet(card1, card2, card3):
-    if card1 is None or card2 is None or card3 is None:
-        return False  
+def matchColor(card1,card2,card3):
      #if colors match
     if (card1.getColor() == card2.getColor() and card2.getColor() == card3.getColor()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def difColor(card1,card2,card3):
     #all different colors
-    elif (card1.getColor() != card2.getColor() and card2.getColor() != card3.getColor() and card1.getColor() != card3.getColor()):
+    if (card1.getColor() != card2.getColor() and card2.getColor() != card3.getColor() and card1.getColor() != card3.getColor()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def matchFill(card1,card2,card3):
     #if fills match
-    elif (card1.getFill() == card2.getFill() and card2.getFill() == card3.getFill()):
+    if (card1.getFill() == card2.getFill() and card2.getFill() == card3.getFill()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def difFill(card1,card2,card3):
     #all different fills
-    elif (card1.getFill() != card2.getFill() and card2.getFill() != card3.getFill() and card1.getFill() != card3.getFill()):
+    if (card1.getFill() != card2.getFill() and card2.getFill() != card3.getFill() and card1.getFill() != card3.getFill()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def matchShape(card1,card2,card3):
     #if shapes match
-    elif (card1.getShape() == card2.getShape() and card2.getShape() == card3.getShape()):
+    if (card1.getShape() == card2.getShape() and card2.getShape() == card3.getShape()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def difShape(card1,card2,card3):
     #all different shapes
-    elif (card1.getShape() != card2.getShape() and card2.getShape() != card3.getShape() and card1.getShape() != card3.getShape()):
+    if (card1.getShape() != card2.getShape() and card2.getShape() != card3.getShape() and card1.getShape() != card3.getShape()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def matchNum(card1,card2,card3):
     #if numbers match
-    elif (card1.getNumber() == card2.getNumber() and card2.getNumber() == card3.getNumber()):
+    if (card1.getNumber() == card2.getNumber() and card2.getNumber() == card3.getNumber()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+def difNum(card1,card2,card3):
     #all different numbers
-    elif (card1.getNumber() != card2.getNumber() and card2.getNumber() != card3.getNumber() and card1.getNumber() != card3.getNumber()):
+    if (card1.getNumber() != card2.getNumber() and card2.getNumber() != card3.getNumber() and card1.getNumber() != card3.getNumber()):
         #messagebox.showinfo("Congrats!", "Good set!")
         return True
+    else:
+        return False
+    
+#takes an input of 3 cards and compares all of their qualities to determine if they form a set or not
+#i added pop up windows to say if it is a good set, or an error for nonvalid set
+def isSet(card1, card2, card3):
+    #3 MATCHES, 1 DISJOINT
+    #color,fill,shape same; number different
+    if ((matchColor(card1,card2,card3)==True) and (matchFill(card1,card2,card3)==True) and (matchShape(card1,card2,card3)==True) and (difNum(card1,card2,card3)==True)):
+        return True
+    #color,shape,number same; fill different
+    elif (matchColor(card1,card2,card3)==True and matchShape(card1,card2,card3)==True and matchNum(card1,card2,card3)==True and difFill(card1,card2,card3)==True):
+        return True
+    #color,fill,number same; shape different
+    elif (matchColor(card1,card2,card3)==True and matchFill(card1,card2,card3)==True and matchNum(card1,card2,card3)==True and difShape(card1,card2,card3)==True):
+        return True
+    #shape,fill,number same; color different
+    elif (matchNum(card1,card2,card3)==True and matchFill(card1,card2,card3)==True and matchShape(card1,card2,card3)==True and difColor(card1,card2,card3)==True):
+        return True
+    
+    #1 MATCH, 3 DISJOINT
+    #color same; shape,fill,number different
+    elif (matchColor(card1,card2,card3)==True and difShape(card1,card2,card3)==True and difFill(card1,card2,card3)==True and difNum(card1,card2,card3)==True):
+        return True
+    #shape same; color,fill,number different
+    elif (matchShape(card1,card2,card3)==True and difColor(card1,card2,card3)==True and difFill(card1,card2,card3)==True and difNum(card1,card2,card3)==True):
+        return True
+    #fill same; color,shape,number different
+    elif (matchFill(card1,card2,card3)==True and difShape(card1,card2,card3)==True and difColor(card1,card2,card3)==True and difNum(card1,card2,card3)==True):
+        return True
+    #number same; color,shape,fill different
+    elif (matchNum(card1,card2,card3)==True and difShape(card1,card2,card3)==True and difFill(card1,card2,card3)==True and difColor(card1,card2,card3)==True):
+        return True
+    
+    #ALL DISJOINT
+    elif (difColor(card1,card2,card3)==True and difFill(card1,card2,card3) and difShape(card1,card2,card3)==True and difNum(card1,card2,card3)==True):
+        return True
+    #not a set
     else:
         #makes an error pop up, not a set
         #messagebox.showerror("Error", "Not a valid set")
         return False
+        
+#lines 53 through 72 from Prof Szecsei Card.py file
+class Card:
+
+    def __init__(self, Color, Shape, Fill, Number):
+        self.color = Color
+        self.fill = Fill
+        self.shape = Shape
+        self.number = Number
+
+    #create the methods for getting the properties of the card
+    def getColor(self):
+        return self.color
+    def getFill(self):
+        return self.fill
+    def getShape(self):
+        return self.shape
+    def getNumber(self):
+        return self.number
+    def getAll(self):
+        return (self.color, self.fill,self.shape,self.number)
